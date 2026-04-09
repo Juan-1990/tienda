1fb444d54370d6a5ded55752c70a9a5f0a9ffc33
Juan-1990 <jcvr081990@gmail.com>
2026-04-08 22:32:14 -0500

    crud adelantado

--- server/controllers/auth.Controller.js
+++ server/controllers/auth.Controller.js
@@ -1,2 +1,55 @@
 const User = require("../models/User")
 const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
+
+//Registro
+exports.register = async (req, res) => {
+    try {
+        const {nombre, correo, telefono, password } = req.body
+
+        const user = new User({
+            nombre,
+            correo,
+            telefono,
+            password: hashedPassword
+        })
+
+        await user.save()
+
+        res.json({message: "Usuario registrado correctamente"})
+
+    } catch(error) {
+        res.status(500).json({ error: error.message})
+    }
+}
+
+// LOGIN
+exports.login = async (req, res) => {
+  try {
+    const { correo, password } = req.body;
+
+    const user = await User.findOne({ correo });
+
+    if (!user) {
+      return res.status(400).json({ message: "Usuario no encontrado" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({ message: "Contraseña incorrecta" });
+    }
+
+    // Crear token
+    const token = jwt.sign(
+      { id: user._id },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" }
+    );
+
+    res.json({ token });
+
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
--- server/index.js
+++ server/index.js
@@ -2,12 +2,14 @@ const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
 require("dotenv").config()
+const authRoutes = require("./routes/authRoutes");
 
 const app = express()
 
 //Middlewares
 app.use(cors())
 app.use(express.json())
+app.use("/api/auth", authRoutes);
 
 //ruta de prueba
 app.get("/", (req, res) => {
@@ -15,12 +17,15 @@ app.get("/", (req, res) => {
 })
 
 // coneccion con mongodb
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
     .then(() => console.log("MongoDB conectado"))
     .catch(err => console.log(err))
 
 // puerto
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en puerto ${PORT}`)
 })
